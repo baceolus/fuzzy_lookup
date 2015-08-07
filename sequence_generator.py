ab691a022ca7e911d82a86b49a688050d1f3f326
baceolus <ivigoral@gmail.com>
2015-08-07 14:05:38 +0300

    add possibility to generate multiple sequences

--- sequence_generator.py
+++ sequence_generator.py
@@ -1,19 +1,20 @@
 import random
 
-def sequences_generator(filename):
+def sequences_generator(filename, sequence_len, sequences_number):
     output = open(filename, 'w')
     nucleotides = ['a', 't', 'g', 'c']
     sequence = ''
 
-    for i in xrange(100):
-        sequence += random.choice(nucleotides)
+    for j in xrange(sequences_number):
+        sequence = ''
+        for i in xrange(sequence_len):
+            sequence += random.choice(nucleotides)
+        sequence += '\n'
+        print sequence
+        print len(sequence)
     
-    sequence += '\n'
-    print sequence
-    print len(sequence)
-    
-    output.write(sequence)
+        output.write(sequence)
 
     output.close()
 
-sequences_generator('yay')
+sequences_generator('yay', 100, 10000)
